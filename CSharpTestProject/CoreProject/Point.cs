7ad28941eeaa99c61a9fceb892f0bafeb2888cd5
Leon Gierschner <gierschner.lg@gmail.com>
2021-02-03 15:10:06 +0100

    point tests

---
+++ CSharpTestProject/CoreProject/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProject
+{
+	public class Point1<T>
+	{
+		public T X;
+		public Point1() => X = default;
+		public Point1(T X = default) => this.X = X;
+	}
+
+	public class Point2<T> : Point1<T>
+	{
+		public T Y;
+		public Point2() : base() => Y = default;
+		public Point2(T X = default, T Y = default) : base(X) => this.Y = Y;
+	}
+
+	public class Point3<T> : Point2<T>
+	{
+		public T Z;
+		public Point3() : base() => Z = default;
+		public Point3(T X = default, T Y = default, T Z = default) : base(X, Y) => this.Z = Z;
+	}
+}
--- CSharpTestProject/UnitTestProject2/LibraryTest.cs
+++ CSharpTestProject/UnitTestProject2/LibraryTest.cs
@@ -1,14 +1,57 @@
-﻿using NUnit.Framework;
+﻿using CoreProject;
+using NUnit.Framework;
 
 namespace UnitTestProject2
 {
 	[TestFixture]
     public class LibraryTest
     {
-        [Test]
-        public void TestInheritance()
-		{
-            Assert.AreEqual(1, 1);
-		}
+	    [Test]
+	    public void Point1Tests()
+	    {
+		    var p1 = new Point1<float>();
+			Assert.AreEqual(0.0f, p1.X);
+			Assert.AreEqual(typeof(float), p1.X.GetType());
+
+			var p2 = new Point1<double>(2.0);
+			Assert.AreEqual(2.0, p2.X);
+			Assert.AreEqual(typeof(double), p2.X.GetType());
+	    }
+
+	    [Test]
+	    public void Point2Tests()
+	    {
+		    var p1 = new Point2<float>();
+		    Assert.AreEqual(0.0f, p1.X);
+		    Assert.AreEqual(0.0f, p1.Y);
+		    Assert.AreEqual(typeof(float), p1.X.GetType());
+		    Assert.AreEqual(typeof(float), p1.Y.GetType());
+
+		    var p2 = new Point2<double>(2.0, 3.5);
+		    Assert.AreEqual(2.0, p2.X);
+		    Assert.AreEqual(3.5, p2.Y);
+		    Assert.AreEqual(typeof(double), p2.X.GetType());
+		    Assert.AreEqual(typeof(double), p2.Y.GetType());
+	    }
+
+	    [Test]
+	    public void Point3Tests()
+	    {
+		    var p1 = new Point3<float>();
+		    Assert.AreEqual(0.0f, p1.X);
+		    Assert.AreEqual(0.0f, p1.Y);
+		    Assert.AreEqual(0.0f, p1.Z);
+		    Assert.AreEqual(typeof(float), p1.X.GetType());
+		    Assert.AreEqual(typeof(float), p1.Y.GetType());
+		    Assert.AreEqual(typeof(float), p1.Z.GetType());
+
+		    var p2 = new Point3<double>(2.0, 3.5, 6.7);
+		    Assert.AreEqual(2.0, p2.X);
+		    Assert.AreEqual(3.5, p2.Y);
+		    Assert.AreEqual(6.7, p2.Z);
+		    Assert.AreEqual(typeof(double), p2.X.GetType());
+		    Assert.AreEqual(typeof(double), p2.Y.GetType());
+		    Assert.AreEqual(typeof(double), p2.Z.GetType());
+	    }
     }
 }
